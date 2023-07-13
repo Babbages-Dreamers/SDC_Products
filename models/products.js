effb8cd56c9f6c6076c492a86a3c0be4dcff609b
davetwo7 <davidolu2019@gmail.com>
2023-07-13 15:47:08 -0400

    Add related model function

--- models/products.js
+++ models/products.js
@@ -14,7 +14,6 @@ const getProductInfo = (product_id) => {
     return product_data;
   })
   .catch((err) =>  err)
-
 }
 
 module.exports = {
--- models/related.js
+++ models/related.js
@@ -1,1 +1,9 @@
+const { db } = require('../database/dataAccess.js')
 
+const getRelatedProducts = (product_id) => {
+  return db.query('SELECT related_product_id FROM related WHERE current_product_id = $1', [product_id])
+}
+
+module.exports = {
+  getRelatedProducts
+}
