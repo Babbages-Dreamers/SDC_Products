acc8a383c61b80ff88e0e3d06ea652b20e1d0ef5
davetwo7 <davidolu2019@gmail.com>
2023-07-13 15:04:20 -0400

    Add styles model functions

--- models/styles.js
+++ models/styles.js
@@ -2,19 +2,24 @@ const { db } = require('../database/dataAccess.js')
 
 const getStyles = async (product_id) => {
   const styles_table = await db.query('SELECT * FROM styles WHERE product_id = $1', [product_id])
-  styles_table.forEach((style, index) => {
-    const skus_table = db.query('SELECT * FROM skus WHERE style_id = $1', [style.style_id])
-    const photos_table = db.query('SELECT * FROM photos WHERE style_id = $1', [style.style_id])
-    Promise.all([skus_table, photos_table])
-    .then(([skus, photos]) => console.log(skus))
+
+  const results = styles_table.map((style, index) => {
+    const skus_table = db.query('SELECT size, quantity FROM skus WHERE style_id = $1', [style.style_id])
+    const photos_table = db.query('SELECT thumbnail_url, url  FROM photos WHERE style_id = $1', [style.style_id])
+
+    return Promise.all([skus_table, photos_table])
+    .then(([skus, photos]) => {
+      style.photos = photos;
+      skus = skus.reduce((obj, value, index) => {
+        obj[index + 37] = value;
+        return obj;
+      }, {})
+      style.skus = skus;
+      return style;
+    })
   })
-  // let skus_table = db.query('SELECT * FROM skys WHERE style_id = $1', [style_id])
-  // let photos_table = db.query('SELECT * FROM photos WHERE style_id = $1', [style_id])
-
-
+  return Promise.all(results)
 }
-
-getStyles(32)
 
 module.exports = {
   getStyles
